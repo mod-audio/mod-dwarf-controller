bc34b2b6c38d406c4efe607d424b7e2c4c2cbcab
Jan <jan@moddevices.com>
2021-03-22 10:49:46 +0100

    remove button action from update popup

--- app/src/system.c
+++ app/src/system.c
@@ -420,10 +420,9 @@ void system_upgrade_cb(void *arg, int event)
         if (item->data.hover == 0)
         {
             uint8_t foot_status = actuator_get_status(foot);
-            uint8_t button_status = actuator_get_status(button);
 
             // check if footswitch is pressed down
-            if (BUTTON_PRESSED(foot_status) || BUTTON_PRESSED(button_status))
+            if (BUTTON_PRESSED(foot_status))
             {
                 //clear all screens
                 screen_clear();
