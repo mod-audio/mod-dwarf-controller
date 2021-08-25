72e0605f9914f08ae919ab33a6349891f3add31d
Jan <jan@moddevices.com>
2021-08-25 16:23:53 +0200

    close keyboard first before saving

--- app/src/mode_popup.c
+++ app/src/mode_popup.c
@@ -385,6 +385,12 @@ void PM_button_pressed(uint8_t button)
     char buffer[30];
     uint8_t i = 0;
 
+    if (g_global_popups[g_current_popup_id].has_naming_input && g_keyboard_toggled) {
+        g_keyboard_toggled = 0;
+        PM_print_screen();
+        return;
+    }
+
     switch (g_current_popup_id)
     {
         case POPUP_SAVE_SELECT_ID:
