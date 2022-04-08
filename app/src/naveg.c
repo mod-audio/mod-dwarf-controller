b05c739b67a94e8920e39f7ac443b63ef13e7369
Jan <jan@moddevices.com>
2022-04-08 14:49:13 +0200

    block menu item changes when popup is open

--- app/src/naveg.c
+++ app/src/naveg.c
@@ -430,13 +430,7 @@ void naveg_enc_enter(uint8_t encoder)
 
         case MODE_TOOL_FOOT:
         case MODE_TOOL_MENU:
-            if (g_menu_popup_active) {
-                if (encoder == 0)
-                    TM_encoder_click(encoder);
-            }
-            else {
-                TM_encoder_click(encoder);
-            }
+            TM_encoder_click(encoder);
         break;
 
         case MODE_BUILDER:
@@ -550,13 +544,7 @@ void naveg_enc_down(uint8_t encoder)
         case MODE_TOOL_FOOT:
         case MODE_TOOL_MENU:
             //pass for tuner/menu/bypass controls
-            if (g_menu_popup_active) {
-                if (encoder == 0)
-                    TM_down(encoder);
-            }
-            else {
-                TM_down(encoder);
-            }
+            TM_down(encoder);
         break;
 
         case MODE_SHIFT:;
@@ -655,13 +643,7 @@ void naveg_enc_up(uint8_t encoder)
         case MODE_TOOL_FOOT:
         case MODE_TOOL_MENU:
             //pass for tuner/menu/bypass controls
-            if (g_menu_popup_active) {
-                if (encoder == 0)
-                    TM_up(encoder);
-            }
-            else {
-                TM_up(encoder);
-            }
+            TM_up(encoder);
         break;
 
         case MODE_SHIFT:;
